51792cd7cc4c4d812baec596a3bfacabe55ea102
emibelmar <emiliobeltran94@gmail.com>
2021-08-21 16:28:28 +0200

    First commit - Hello World

---
+++ hello-world/src/test/java/com/ebmdev/hello/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebmdev.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
